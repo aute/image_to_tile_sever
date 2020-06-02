37ecd1c6feb615eb808cf8f13eb9f2813913b94d
aute <aute7527@gmail.com>
2020-06-02 11:26:03 +0800

    add log and fix alpha background

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
 const sharp = require('sharp');
 const express = require('express');
 const fs = require('fs');
-const file_name = f => f.replace(/(.*\/)*([^.]+).*/ig, "$2");
 
 const PORT = 8080;
+const tile_opt = {
+    layout: 'google',
+    background: { r: 0, g: 0, b: 0, alpha: 0 }
+};
+const get_file_name = f => f.replace(/(.*\/)*([^.]+).*/ig, "$2");
 
-const app = express();
-app.use(express.static('./files/output'));
-app.use(express.json());
-
-app.put('/original_image', (req, res) => {
-    const image = sharp(`files/input/${req.body.original_image_file_name}`, { limitInputPixels: 0 });
+const putCallback = (req, res) => {
+    const original_name = req.body.original_image_file_name;
+    const file_name = get_file_name(original_name);
+    const image = sharp(`files/input/${original_name}`, { limitInputPixels: 0 });
+    const writeFile = (err, info) => {
+        fs.writeFile(
+            `files/output/${file_name}/info${info ? 'info' : 'error'}.json`,
+            JSON.stringify(info || err),
+            error => console.log(`output ${file_name}:${JSON.stringify(error || err || info)}`)
+        );
+    };
     image.metadata().
-        then(function (metadata) {
+        then((metadata) => {
             res.status(200).send(metadata);
+            console.log(`input ${file_name}:${JSON.stringify(metadata)}`);
             image
                 .png()
-                .tile({
-                    layout: 'google',
-                    background: { r: 0, g: 0, b: 0, alpha: 0 }
-                })
-                .toFile(`files/output/${file_name(req.body.original_image_file_name)}`, function (err, info) {
-                    fs.writeFile(`files/output/${file_name(req.body.original_image_file_name)}/info.json`, JSON.stringify(info), function (err) {
-                        if (err) {
-                            console.error(err);
-                        }
-                        console.log(info);
-                    });
-                });
-        }).catch(function (err) {
+                .ensureAlpha()
+                .tile(tile_opt)
+                .toFile(`files/output/${file_name}`, writeFile);
+        }).catch((err) => {
             console.error(err);
             res.status(400).send(err.message);
         });
-});
+};
 
+const app = express();
+app.use(express.json(), express.static('./files/output'));
+app.put('/original_image', putCallback);
 app.listen(PORT);
 console.log(`tile server start`);
